c331f355d3b7ab0bdc793d7e8c965839cb94b6e1
Vanya351 <goloborodko2005@gmail.com>
2025-02-27 20:22:44 +0300

    Добавлен макет главного меню.

--- Frontend/MainWindow.xaml.cs
+++ Frontend/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Frontend.Windows;
 
 namespace Frontend;
 
@@ -19,5 +20,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        MainFrame.Content = new Auth();
     }
 }
