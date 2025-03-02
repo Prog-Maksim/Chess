2254e2c32aa08cc90eaee8156554fbc9410e0c13
maksim beloglazov <maksimbel20170@gmail.com>
2025-03-02 23:43:56 +0300

    Добавлена возможность войти созданную в игру

---
+++ Frontend/Controls/SetGameIdControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Frontend.Windows;
+
+namespace Frontend.Controls;
+
+public partial class SetGameIdControl : UserControl
+{
+    private readonly MainMenu.GetGameId _gameId;
+    
+    public SetGameIdControl(MainMenu.GetGameId gameId)
+    {
+        InitializeComponent();
+        
+        _gameId = gameId;
+    }
+
+    private void Click_OnClick(object sender, RoutedEventArgs e)
+    {
+        _gameId(GameIdTextBox.Text);
+    }
+}
