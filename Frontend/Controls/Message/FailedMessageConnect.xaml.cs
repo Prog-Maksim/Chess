3f4436b717fd77d001e9431e69b315612b480910
maksim beloglazov <maksimbel20170@gmail.com>
2025-03-03 19:14:57 +0300

    Добавлено уведомление об ошибке подключиться к серверу

---
+++ Frontend/Controls/Message/FailedMessageConnect.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using Frontend.Script;
+using Frontend.Windows;
+
+namespace Frontend.Controls.Message;
+
+public partial class FailedMessageConnect : UserControl, INotify
+{
+    private readonly MainMenu.RetryConnect _deleteNotify;
+    
+    public FailedMessageConnect(MainMenu.RetryConnect deleteNotify)
+    {
+        InitializeComponent();
+        _deleteNotify = deleteNotify;
+    }
+
+    private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        _deleteNotify();
+    }
+}
