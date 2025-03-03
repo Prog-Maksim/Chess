f5e1276fa49dd344fc843e77e8799dfcee778f29
maksim beloglazov <maksimbel20170@gmail.com>
2025-03-03 08:14:15 +0300

    реализовано автоматическое переподключение к бекенду

--- Frontend/URL.cs
+++ Frontend/URL.cs
@@ -2,5 +2,9 @@
 
 public static class Url
 {
+    // public const string BaseUrl = "http://localhost:5064/api/v1/";
+    // public const string BaseUrlWs = "ws://localhost:5064/api/v1/Game/connect";
+    
+    public const string BaseUrlWs = "wss://monitor-advanced-suddenly.ngrok-free.app/api/v1/Game/connect";
     public const string BaseUrl = "https://monitor-advanced-suddenly.ngrok-free.app/api/v1/";
 }
